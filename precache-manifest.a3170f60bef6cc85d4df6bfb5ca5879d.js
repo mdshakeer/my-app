9399ab14e0ee56a3e7e64176230366cc9a5131ae
shakeer <mdshakeer91@gmail.com>
2019-07-29 00:50:31 +0530

    Updates

---
+++ precache-manifest.a3170f60bef6cc85d4df6bfb5ca5879d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d08be13b95684b536b8b5801e9b90b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "6c8bc593864dd2b52dc2",
+    "url": "/my-app/static/css/main.35274ec9.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/my-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "6c8bc593864dd2b52dc2",
+    "url": "/my-app/static/js/main.7e583d19.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "3108d1c8d5f6683de2a6d798066383ab",
+    "url": "/my-app/static/media/shakeer.3108d1c8.jpg"
+  }
+]);
